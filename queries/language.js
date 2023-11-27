909eb490c0a04d514ef0c41eb81443dd376a6925
morcsanyitamas <mtomi8@gmail.com>
2023-11-27 15:10:08 +0100

    add reference examples

--- queries/language.js
+++ queries/language.js
@@ -11,20 +11,81 @@ async function readLanguages() {
   return languages;
 }
 
-async function getLanguages() {
+async function getLanguages(search) {
+  let languages = await readLanguages();
+  
+  // filter
+  if (search) {
+    languages = languages.filter((lang) => {
+      return (
+        lang.name.toLowerCase().includes(search.toLowerCase()) ||
+        lang.maintainer.toLowerCase().includes(search.toLowerCase()) ||
+        lang.designer.toLowerCase().includes(search.toLowerCase())
+      );
+    });
+  }
+  
 
+
+  return languages;
 }
 
 async function getLanguage(langid) {
-
+  const languages = await readLanguages();
+  const response = languages.find((lang) => lang.langid === parseInt(langid));
+  if (response) {
+    return response;
+  } else {
+    throw new Error(`Language with id ${langid} does not exist`);
+  }
 }
 
 async function createLanguage(lang) {
+  const languages = await readLanguages();
+  if(!languages.find((language) => language.langid === lang.langid)){
+    languages.push(lang);
+    await fs.writeFile(languageFilePath, JSON.stringify(languages));
+    return lang;
+  } else {
+    throw new Error(`Language with id ${lang.langid} already exist`);
+  }
+}
 
+async function replaceLanguage(id, lang) {
+  const languages = await readLanguages();
+  const index = languages.findIndex((l) => l.langid === parseInt(id));
+  if (index !== -1) {
+    const newLang = {langid: parseInt(id), ...lang};
+    languages[index] = newLang;
+    await fs.writeFile(languageFilePath, JSON.stringify(languages));
+    return newLang;
+  } else {
+    throw new Error(`Language with id ${lang.langid} does not exist`);
+  }
+}
+
+async function patchLanguage(id, lang) {
+  const languages = await readLanguages();
+  const index = languages.findIndex((l) => l.langid === parseInt(id));
+  if (index !== -1) {
+    for (const key in lang) {
+      if (languages[index][key]) {
+        languages[index][key] = lang[key];
+      } else {
+        throw new Error(`Language with id ${lang.langid} does not has existing ${key} property`);
+      }
+    }
+    await fs.writeFile(languageFilePath, JSON.stringify(languages));
+    return lang;
+  } else {
+    throw new Error(`Language with id ${lang.langid} does not exist`);
+  }
 }
 
 module.exports = {
   getLanguages,
   getLanguage,
   createLanguage,
+  replaceLanguage,
+  patchLanguage
 };
--- server.js
+++ server.js
@@ -1,5 +1,77 @@
+const express = require('express');
+const languageQuery = require('./queries/language');
+const feedbackQuery = require('./queries/feedback');
+
+
+const app = express();
+const PORT = 4000;
+
+app.use(express.json());
+app.use(logger);
+
+app.get('/', (req, res) => {
+  res.send('Popular programming languages API 2.0.');
+});
+
+app.get('/api/v2/languages', async (req, res, next) => {
+  const { search } = req.query;
+
+  try {
+    const languages = await languageQuery.getLanguages(search);
+    res.status(200).send(languages);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+
+app.get("/api/v2/languages/:langid", async (req, res) => {
+  try {
+    const language = await languageQuery.getLanguage(req.params.langid);
+    res.status(200).send(language);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.post("/api/v2/languages", async (req, res) => {
+  try {
+    const language = await languageQuery.createLanguage(req.body);
+    res.status(200).send(language);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.put("/api/v2/languages/:langid", async (req, res) => {
+  try {
+    const language = await languageQuery.replaceLanguage(req.params.langid, req.body);
+    res.status(200).send(language);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.patch("/api/v2/languages/:langid", async (req, res) => {
+  try {
+    const language = await languageQuery.patchLanguage(req.params.langid, req.body);
+    res.status(200).send(language);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
 function logger(req, res, next) {
   const ts = new Date();
   console.log(`[${ts}]: ${req.method} ${req.originalUrl}`);
   next();
 }
+
+
+function main(){
+  app.listen(PORT, () => {
+    console.log(`Listening on http://localhost:${PORT}`);
+  });
+}
+
+main();
